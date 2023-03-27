219bd3411f0ac8debd735302d4c1d4fe256e7684
Ankit Kumar <ankitkumar95993@gmail.com>
2023-03-27 22:47:31 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import "./App.css";
+import React from "react";
+import Navbar from  "./components/Navbar";
+import Cards from "./components/Cards"
+import Filter from "./components/Filter"
+import { apiUrl, filterData  } from "./data";
+import { useState,useEffect } from "react";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
 
-function App() {
+
+const App = () => { 
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output -> 
+      setCourses(output.data);
+    }
+    catch(error) {
+        toast.error("Network me koi dikkat hai");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    <div className="min-h-screen flex flex-col bg-white">
+      <div>
+        <Navbar/>
+      </div>
+      <div className="bg-bgDark2">
+        <div>
+          <Filter 
+          filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+          />
+        </div>
+        <div className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+        {
+            loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
+          }
+        </div>
+      </div>
+
+
+    </div>
   );
-}
+};
 
 export default App;
